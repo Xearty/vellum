1492d09a49bbcaae4d3bf3becb2740793c77231c
Martin Ivanov <xeartybg@gmail.com>
2025-07-01 00:43:26 +0300

    Install and setup `nerd-icons`
    Setup nerd icons for the following places
    - dired
    - ibuffer
    - vertico + marginalia
    - corfu

---
+++ plugins/nerd-icons.el
@@ -0,0 +1,19 @@
+(use-package nerd-icons)
+
+(use-package nerd-icons-dired
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+(use-package nerd-icons-ibuffer
+  :ensure t
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
+
+(use-package nerd-icons-completion
+  :after marginalia
+  :config
+  (nerd-icons-completion-mode)
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+
+(use-package nerd-icons-corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
