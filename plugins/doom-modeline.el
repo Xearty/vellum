a56c17488ab1dc77fb156db6462491399ca35646
Martin Ivanov <xeartybg@gmail.com>
2025-07-01 00:42:41 +0300

    Change `doom-modeline` buffer file name style to `relative-from-project`

--- plugins/doom-modeline.el
+++ plugins/doom-modeline.el
@@ -1,5 +1,6 @@
 (use-package doom-modeline
   :init
+  (setq doom-modeline-buffer-file-name-style 'relative-from-project)
   (setq doom-modeline-hud t)
   (setq doom-modeline-buffer-modification-icon nil)
   ;; (setq doom-modeline-position-line-format nil)
