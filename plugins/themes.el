92272740d8cf411ac340aa8d38aa45d9632471c1
Martin Ivanov <xeartybg@gmail.com>
2025-07-01 23:43:20 +0300

    Install `solaire-mode`

--- plugins/themes.el
+++ plugins/themes.el
@@ -3,6 +3,9 @@
   (setq modus-themes-common-palette-overrides
       '((fg-region unspecified))))
 
-
 (use-package doom-themes)
 (use-package vscode-dark-plus-theme)
+
+(use-package solaire-mode
+  :config
+  (solaire-global-mode +1))
