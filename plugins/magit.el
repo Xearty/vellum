805aa1b47ebbebd742252cb35d474f4897d07d75
Martin Ivanov <xeartybg@gmail.com>
2025-07-02 21:40:55 +0300

    Setup magit to take the whole frame and restore the window configuration afterwards

--- plugins/magit.el
+++ plugins/magit.el
@@ -1,3 +1,5 @@
 (use-package magit
   :config
+  (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1
+	magit-bury-buffer-function 'magit-restore-window-configuration)
   (setq magit-save-repository-buffers nil))
