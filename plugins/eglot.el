d0c601a582ab5867ea903faa0f5a64a3d06d6561
Martin Ivanov <xeartybg@gmail.com>
2025-07-03 01:17:45 +0300

    Setup C and C++ mode and add sources for TS grammars

--- plugins/eglot.el
+++ plugins/eglot.el
@@ -5,7 +5,9 @@
 	eldoc-echo-area-use-multiline-p nil
 	eglot-put-doc-in-help-buffer 'eglot-doc-too-large-for-echo-area)
   (add-hook 'eglot-managed-mode-hook (lambda () (eglot-inlay-hints-mode -1))) 
-  :hook ((rust-mode rust-ts-mode) . eglot-ensure)
+  :hook (((rust-mode rust-ts-mode) . eglot-ensure)
+	 ((c-mode c-ts-mode) . eglot-ensure)
+	 ((c++-mode C++-ts-mode) . eglot-ensure))
   :bind ( :map rust-mode-map
 	  ("C-c l i h" . eglot-inlay-hints-mode)))
 
--- plugins/treesit.el
+++ plugins/treesit.el
@@ -5,7 +5,8 @@
      (rust "https://github.com/tree-sitter/tree-sitter-rust" "v0.23.0")
      (json "https://github.com/tree-sitter/tree-sitter-json" "v0.24.8")
      (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "v0.23.2")
-     (toml "https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1")))
+     (toml "https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1")
+     (cpp "https://github.com/tree-sitter/tree-sitter-cpp" "v0.23.3")))
 
 ; Lazy installing of language grammars
 (dolist (source treesit-language-source-alist)
