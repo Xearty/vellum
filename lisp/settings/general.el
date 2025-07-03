ed9d1224b715d83a49d3e4dc3957d751387b3a4c
Martin Ivanov <xeartybg@gmail.com>
2025-07-03 18:46:50 +0300

    Disable `auto-save-mode`

--- lisp/settings/general.el
+++ lisp/settings/general.el
@@ -1,6 +1,10 @@
 (save-place-mode 1)
 (global-auto-revert-mode 1)
 
+(setq auto-save-no-message t)
+(setq auto-save-default nil)
+(auto-save-mode -1)
+
 (setq gc-cons-threshold 50000000)
 (setq large-file-warning-threshold 100000000)
 (setq global-auto-revert-non-file-buffers t)
