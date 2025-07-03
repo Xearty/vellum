64ee7acd361daa7df28b50bc45669ddf406e9857
Martin Ivanov <xeartybg@gmail.com>
2025-07-03 18:14:19 +0300

    Setup `markdown-mode` keybindings to toggle view and edit modes

--- plugins/markdown-mode.el
+++ plugins/markdown-mode.el
@@ -1,3 +1,21 @@
 (use-package markdown-mode
   :mode ("README\\.md\\'" . gfm-mode)
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command "multimarkdown")
+  :config
+  (define-key markdown-view-mode-map (kbd "C-c d v") (lambda ()
+						  (interactive)
+						  (markdown-mode)))
+
+  (define-key markdown-mode-map (kbd "C-c d v") (lambda ()
+						  (interactive)
+						  (markdown-view-mode)))
+
+  (define-key gfm-mode-map (kbd "C-c d v") (lambda ()
+					     (interactive)
+					     (gfm-view-mode)))
+
+  (define-key gfm-view-mode-map (kbd "C-c d v") (lambda ()
+						  (interactive)
+						  (gfm-mode)))
+
+(markdown-toggle-fontify-code-blocks-natively +1))
