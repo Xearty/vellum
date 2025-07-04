2947b69e693c9db39fed6b17b6228a99bd2a0e14
Martin Ivanov <xeartybg@gmail.com>
2025-07-04 15:20:37 +0300

    Bind `dirvish-dwim` and rebing `dirvish`

--- plugins/dirvish.el
+++ plugins/dirvish.el
@@ -2,7 +2,8 @@
   :after evil
   :init
   (with-eval-after-load 'evil
-    (evil-global-set-key 'motion (kbd "-") 'dirvish))
+    (evil-global-set-key 'motion (kbd "_") 'dirvish)
+    (evil-global-set-key 'motion (kbd "-") 'dirvish-dwim))
   :config
   (with-eval-after-load 'evil
     (define-key dirvish-mode-map [remap quit-window] 'dirvish-quit)
