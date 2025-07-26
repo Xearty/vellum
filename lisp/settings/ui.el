50274b0020cec811f3fedcb1bbe12c533ef3d622
Martin Ivanov <xeartybg@gmail.com>
2025-07-26 23:11:58 +0300

    Shrink font size

--- lisp/settings/ui.el
+++ lisp/settings/ui.el
@@ -6,7 +6,7 @@
 ;; Maximize on startup
 (add-hook 'window-setup-hook 'toggle-frame-maximized t)
 
-(set-face-attribute 'default nil :family "JetBrains Mono" :height 160)
+(set-face-attribute 'default nil :family "JetBrains Mono" :height 150)
 
 (setq use-dialog-box nil)
 
