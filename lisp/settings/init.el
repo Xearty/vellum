1acaab82e7c877685ed85a461841ddab1242b983
Martin Ivanov <xeartybg@gmail.com>
2025-08-04 23:51:26 +0300

    Copy Casey Muratori's C++ Big fun hook

--- lisp/settings/init.el
+++ lisp/settings/init.el
@@ -1,4 +1,4 @@
 (let ((current-dir (file-name-directory load-file-name))
-      (modules '("ui.el" "general.el" "keymaps.el" "themes.el")))
+      (modules '("ui.el" "general.el" "keymaps.el" "themes.el" "prog.el")))
   (dolist (module modules) (load-file (concat current-dir module))))
       
---
+++ lisp/settings/prog.el
@@ -0,0 +1,73 @@
+; C++ indentation style
+(defconst casey-big-fun-c-style
+  '((c-electric-pound-behavior   . nil)
+    (c-tab-always-indent         . t)
+    (c-comment-only-line-offset  . 0)
+    (c-hanging-braces-alist      . ((class-open)
+                                    (class-close)
+                                    (defun-open)
+                                    (defun-close)
+                                    (inline-open)
+                                    (inline-close)
+                                    (brace-list-open)
+                                    (brace-list-close)
+                                    (brace-list-intro)
+                                    (brace-list-entry)
+                                    (block-open)
+                                    (block-close)
+                                    (substatement-open)
+                                    (statement-case-open)
+                                    (class-open)))
+    (c-hanging-colons-alist      . ((inher-intro)
+                                    (case-label)
+                                    (label)
+                                    (access-label)
+                                    (access-key)
+                                    (member-init-intro)))
+    (c-cleanup-list              . (scope-operator
+                                    list-close-comma
+                                    defun-close-semi))
+    (c-offsets-alist             . ((arglist-close         .  c-lineup-arglist)
+                                    (label                 . -4)
+                                    (access-label          . -4)
+                                    (substatement-open     .  0)
+                                    (statement-case-intro  .  4)
+                                    (statement-block-intro .  c-lineup-for)
+                                    (case-label            .  4)
+                                    (block-open            .  0)
+                                    (inline-open           .  0)
+                                    (topmost-intro-cont    .  0)
+                                    (knr-argdecl-intro     . -4)
+                                    (brace-list-open       .  0)
+                                    (brace-list-intro      .  4)))
+    (c-echo-syntactic-information-p . t))
+    "Casey's Big Fun C++ Style")
+
+; CC++ mode handling
+(defun casey-big-fun-c-hook ()
+  ; Set my style for the current buffer
+  (c-add-style "BigFun" casey-big-fun-c-style t)
+
+  ; 4-space tabs
+  (setq tab-width 4
+        indent-tabs-mode nil)
+
+  ; Additional style stuff
+  (c-set-offset 'member-init-intro '++)
+
+  ; No hungry backspace
+  (c-toggle-auto-hungry-state -1)
+
+  ; Newline indents, semi-colon doesn't
+  (define-key c++-mode-map "\C-m" 'newline-and-indent)
+  (setq c-hanging-semi&comma-criteria '((lambda () 'stop)))
+
+  ; Handle super-tabbify (TAB completes, shift-TAB actually tabs)
+  (setq dabbrev-case-replace t)
+  (setq dabbrev-case-fold-search t)
+  (setq dabbrev-upcase-means-case-search t)
+
+  ; Abbrevation expansion
+  (abbrev-mode 1))
+
+(add-hook 'c-mode-common-hook 'casey-big-fun-c-hook)
