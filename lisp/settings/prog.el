9b32d0bc70ef9325bd56512ef43c96294278e8ef
Martin Ivanov <xeartybg@gmail.com>
2025-08-04 23:58:20 +0300

    Casey's colored TODO and NOTE

--- lisp/settings/prog.el
+++ lisp/settings/prog.el
@@ -71,3 +71,17 @@
   (abbrev-mode 1))
 
 (add-hook 'c-mode-common-hook 'casey-big-fun-c-hook)
+
+
+; Bright-red TODOs
+(setq fixme-modes '(c++-mode c-mode emacs-lisp-mode))
+(make-face 'font-lock-fixme-face)
+(make-face 'font-lock-note-face)
+(mapc (lambda (mode)
+	(font-lock-add-keywords
+	 mode
+	 '(("\\<\\(TODO\\)" 1 'font-lock-fixme-face t)
+	   ("\\<\\(NOTE\\)" 1 'font-lock-note-face t))))
+      fixme-modes)
+(modify-face 'font-lock-fixme-face "Red" nil nil t nil t nil nil)
+(modify-face 'font-lock-note-face "Dark Green" nil nil t nil t nil nil)
