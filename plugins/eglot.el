7e2288b3fad955f2ef71631fa3d72a55373766d3
Martin Ivanov <xeartybg@gmail.com>
2025-08-21 16:52:41 +0300

    Disable clangd

--- plugins/eglot.el
+++ plugins/eglot.el
@@ -31,8 +31,9 @@
   (add-hook 'eglot-managed-mode-hook (lambda () (eglot-inlay-hints-mode -1))) 
 
   :hook (((rust-mode rust-ts-mode) . eglot-ensure)
-	 ((c-mode c-ts-mode) . eglot-ensure)
-	 ((c++-mode C++-ts-mode) . eglot-ensure))
+	 ;; ((c-mode c-ts-mode) . eglot-ensure)
+	 ;; ((c++-mode C++-ts-mode) . eglot-ensure)
+	 )
 
   :bind ( :map eglot-mode-map
 	  ("C-c l i h" . eglot-inlay-hints-mode)
